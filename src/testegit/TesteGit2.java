dc1375cb49faaf585f1bd0c770555e71b233d6b6
guest01 <guest01@10.1.48.218>
2013-09-29 20:56:14 -0300

    Add Nova classe

---
+++ src/testegit/TesteGit2.java
@@ -0,0 +1,30 @@
+package testegit;
+
+public class TesteGit2 {
+    private String Nome;
+    private int idade;
+
+    public String getNome() {
+        return Nome;
+    }
+
+   
+    
+    
+    
+    
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+    
+}
